40439b8e2da4b2aa8dec96d55f0b8f3a6f561e4f
radizorit <jlimanalysis@gmail.com>
2022-09-14 02:28:41 -0700

    need to fix dropdown for dates

--- src/components/SearchOptions.js
+++ src/components/SearchOptions.js
@@ -1,64 +1,51 @@
-import React, { useState, useRef } from 'react'
-import { Grid, Button, Typography, Card, TextField } from '@mui/material';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import React, { useState } from 'react';
+import dayjs from 'dayjs';
 
-import { getPolygonData } from '../controllers/polygonData'
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
 
-const SearchOptions = () => {
-    const [searchCriteria, setSearchCriteria] = useState({
-        ticker: 'AAPL',
-        start: '2021-08-01',
-        end: new Date().getMonth() + '/' + new Date().getDate() + '/' + new Date().getFullYear(),
-        timeframe: 'day'
-    })
 
+export default function SearchOptions() {
+    const [ticker, setTicker] = useState('APPL');
 
-    const handleSubmit = (e) => {
-        e.preventDefault(); //this prevents refreshing page
-        getPolygonData(searchCriteria)
-    }
+    const [start, setStart] = useState(
+        dayjs('2022-08-18T21:11:54')
+    );
+
+    const [end, setEnd] = useState(
+        dayjs('2022-09-14T21:11:54')
+    );
 
     return (
-        <Card style={{ maxWidth: 450, padding: "20px 40px 0px 0px" }}>
-            <Typography variant='h3'>Send a message!</Typography>
-            <form onSubmit={handleSubmit} style={{ maxWidth: 450, padding: "20px 5px", margin: "0 auto" }}>
-                <ul>
-                    <TextField
-                        onChange={(e) => setSearchCriteria({ ...searchCriteria, ticker: e.target.value })}
-                        label='Ticker'
-                        // variant='outlined'
-                        // color='secondary'
-                        fullWidth
-                    />
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DesktopDatePicker
-                            label="Start"
-                            inputFormat="MM/DD/YYYY"
-                            value={searchCriteria['start']}
-                            onChange={(e) => setSearchCriteria({ ...searchCriteria, start: e.target.value })}
-                            renderInput={(params) => <TextField {...params} />}
-                        />
-                        <DesktopDatePicker
-                            label="End"
-                            inputFormat="MM/DD/YYYY"
-                            value={searchCriteria['end']}
-                            onChange={(e) => setSearchCriteria({ ...searchCriteria, end: e.target.value })}
-                            renderInput={(params) => <TextField {...params} />}
-                        />
-                    </LocalizationProvider>
-                    <TextField
-                        onChange={(e) => setSearchCriteria({ ...searchCriteria, timeframe: e.target.value })}
-                        label='Range'
-                        variant='outlined'
-                        color='secondary'
-                        fullWidth
-                    />
-                    <Button onClick={(e) => handleSubmit(e)}>Click me to submit</Button>
-                </ul>
-            </form>
-        </Card>
+// fix the date, it keeps going to most recent date
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <Stack spacing={3}>
+                <TextField
+                    onChange={(e) => setTicker()}
+                    value={ticker}
+                    label='Ticker'
+                    variant='outlined'
+                    color='secondary'
+                    fullWidth
+                />
+                <DesktopDatePicker
+                    label="Start Date"
+                    inputFormat="YYYY-MM-DD"
+                    value={start}
+                    onChange={(e) => setStart()}
+                    renderInput={(params) => <TextField {...params} />}
+                />
+                <DesktopDatePicker
+                    label="End Date"
+                    inputFormat="YYYY-MM-DD"
+                    value={end}
+                    onChange={(e) => setEnd()}
+                    renderInput={(params) => <TextField {...params} />}
+                />
+            </Stack>
+        </LocalizationProvider>
     );
 }
-
-export default SearchOptions
